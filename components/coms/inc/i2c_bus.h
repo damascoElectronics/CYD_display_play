784dd10d1f353e5972ecfbb434cd7fb30788b87d
David Ricardo Hernandez <ing.david.r.hernandez@gmail.com>
2026-04-27 09:17:54 +0200

    feat: setting up I2C bus

---
+++ components/coms/inc/i2c_bus.h
@@ -0,0 +1,12 @@
+/**
+ * @file i2c_bus.h
+ * @brief .
+ */
+#ifndef I2C_BUS_H
+#define I2C_BUS_H
+
+// TEF6686 — I2C
+#define BSP_TEF6686_SDA  GPIO_NUM_27
+#define BSP_TEF6686_SCL  GPIO_NUM_22
+
+#endif
